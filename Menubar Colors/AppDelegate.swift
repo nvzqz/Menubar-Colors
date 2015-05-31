7f0f5a47f05ba8d22209c393e886eb09c9c8a0fd
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-05-30 20:56:37 -0400

    Initial Commit

---
+++ Menubar Colors/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Menubar Colors
+//
+//  Created by Nikolai Vazquez on 5/30/15.
+//  Copyright (c) 2015 Nikolai Vazquez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
