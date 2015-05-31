06c2d702a77594dcf90507d04f18ee9ca28764de
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-05-31 04:19:30 -0400

    Set left and right click functionality apart

--- Menubar Colors/AppDelegate.swift
+++ Menubar Colors/AppDelegate.swift
@@ -9,18 +9,52 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     
     @IBOutlet weak var statusMenu: NSMenu!
     
-    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
-
+    let colorPanel = NSColorPanel()
+    
+    var statusItem: NSStatusItem?
+    var statusButton: NSStatusBarButton?
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
+        //Set icon image
         let icon = NSImage(named: "statusIcon")
         icon!.setTemplate(true)
+        statusItem!.image = icon
         
-        statusItem.image = icon
-        statusItem.menu = statusMenu
+    }
+    
+    override func awakeFromNib() {
+        //Set status bar item to default size
+        let statusBar = NSStatusBar.systemStatusBar()
+        statusItem = statusBar.statusItemWithLength(-1)
+        
+        statusButton = statusItem!.button!
+        statusButton?.target = self
+        statusButton?.action = "statusButtonPressed:"
+        statusButton?.sendActionOn(Int((NSEventMask.LeftMouseUpMask | NSEventMask.RightMouseUpMask).rawValue))
+        
+        statusMenu.delegate = self
+    }
+    
+    func statusButtonPressed(sender: NSStatusBarButton!) {
+        var event: NSEvent! = NSApp.currentEvent!
+        if (event.type == NSEventType.RightMouseUp) {
+            statusItem?.menu = statusMenu
+            statusItem?.popUpStatusItemMenu(statusMenu)
+        } else {
+            openColorPicker()
+        }
+    }
+    
+    //Disable left click menu upon close
+    func menuDidClose(menu: NSMenu) {
+        statusItem?.menu = nil
+    }
+    
+    func openColorPicker() {
         
     }
 
@@ -29,7 +63,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     @IBAction func colorsMenuItemSelected(sender: NSMenuItem) {
-        // Open the color picker
+        openColorPicker()
     }
     
 
