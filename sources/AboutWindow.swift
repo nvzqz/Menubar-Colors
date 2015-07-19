e33f7d72b1bd96f07860160e804275d7e0dd4e92
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-07-19 19:02:20 -0400

    Create About window class, nib, and controller

---
+++ sources/AboutWindow.swift
@@ -0,0 +1,13 @@
+//
+//  AboutWindow.swift
+//  Menubar Colors
+//
+//  Created by Nikolai Vazquez on 7/19/15.
+//  Copyright (c) 2015 Nikolai Vazquez. All rights reserved.
+//
+
+import Cocoa
+
+class AboutWindow: NSWindow {
+
+}
---
+++ sources/AboutWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutWindowController.swift
+//  Menubar Colors
+//
+//  Created by Nikolai Vazquez on 7/19/15.
+//  Copyright (c) 2015 Nikolai Vazquez. All rights reserved.
+//
+
+import Cocoa
+
+class AboutWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
