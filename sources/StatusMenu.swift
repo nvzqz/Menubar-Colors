232643e1b97b1fc5dbfb381af2e8b1d70ca8af55
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-07-19 20:38:30 -0400

    Add StatusMenu subclass of NSMenu
    This will store properties and variables from Interface Builder such as
    specific menu items and sub menus.

---
+++ sources/StatusMenu.swift
@@ -0,0 +1,13 @@
+//
+//  ColorsMenu.swift
+//  Menubar Colors
+//
+//  Created by Nikolai Vazquez on 7/19/15.
+//  Copyright (c) 2015 Nikolai Vazquez. All rights reserved.
+//
+
+import Cocoa
+
+class StatusMenu: NSMenu {
+
+}
