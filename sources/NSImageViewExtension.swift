86e2c746629b243d88315799f054c5926a719418
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-07-21 00:37:03 -0400

    Extend NSImageView to override variables
    When dragging starting from the app icon in the About window, the window
    wouldn't drag.
    
    Overriding the mouseDownCanMoveWindow variable to return true allows the
    user to drag the window around even when selecting over the image.

---
+++ sources/NSImageViewExtension.swift
@@ -0,0 +1,17 @@
+//
+//  DraggableImageView.swift
+//  Menubar Colors
+//
+//  Created by Nikolai Vazquez on 7/21/15.
+//  Copyright (c) 2015 Nikolai Vazquez. All rights reserved.
+//
+
+import Cocoa
+
+extension NSImageView {
+    
+    override public var mouseDownCanMoveWindow: Bool {
+        return true
+    }
+    
+}
