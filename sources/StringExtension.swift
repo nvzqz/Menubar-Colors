89f669cdbacfbb3b5940aa867efb00296874cd87
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-08-27 00:54:19 -0400

    Add NS property to String
    Returns self as NSString.

---
+++ sources/StringExtension.swift
@@ -0,0 +1,15 @@
+//
+//  StringExtension.swift
+//  Menubar Colors
+//
+//  Created by Nikolai Vazquez on 8/27/15.
+//  Copyright © 2015 Nikolai Vazquez. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var NS: NSString {
+        return self as NSString
+    }
+}
